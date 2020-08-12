60deb793b795cb6d5a7b495af63b262a5bfa27ff
John Franey <johnfraney@gmail.com>
2020-08-12 20:34:55 -0300

    WIP

---
+++ typescript-synchronize-drf-vue-part-2/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h1>User</h1>
+    <form>
+      <div
+        v-for="(field, fieldName) in user"
+        :key="fieldName"
+      >
+        <label v-text="field.label" />
+        <input
+          :name="fieldName"
+          :required="field.required"
+        >
+      </div>
+    </form>
+    <pre v-text="user" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import User from  './metadata/User.json'
+
+export default Vue.extend({
+  name: 'App',
+
+  data() {
+    return {
+      user: User,
+    }
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
