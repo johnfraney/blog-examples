e3acabf66a843b8527a50ca3ee16ed0bae515edf
John Franey <johnfraney@gmail.com>
2020-08-15 21:26:17 -0300

    WIP

--- typescript-synchronize-drf-vue-part-2/src/App.vue
+++ typescript-synchronize-drf-vue-part-2/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <h1>User</h1>
-    <form>
-      <div
-        v-for="(field, fieldName) in user"
-        :key="fieldName"
-      >
-        <label v-text="field.label" />
+    <h1>User Form</h1>
+    <form @submit.prevent="onSubmit">
+      <div v-for="(inputData, inputName) in fields" :key="inputName">
+        <label v-text="inputData.label" />
         <input
-          :name="fieldName"
-          :required="field.required"
+          v-model="form[inputName]"
+          v-bind="inputData.inputAttributes"
         >
       </div>
+      <button type="submit">Submit</button>
     </form>
-    <pre v-text="user" />
   </div>
 </template>
 
@@ -22,13 +19,43 @@ import Vue from 'vue';
 
 import User from  './metadata/User.json'
 
+function convertFieldMetadataToInputData(fieldMetadata: any) {
+  const label = fieldMetadata.label
+  const inputAttributes: {[key: string]: string | number | boolean} = {
+    maxlength: fieldMetadata.max_length,
+    name: fieldMetadata.field_name,
+    required: fieldMetadata.required,
+    type: fieldMetadata.type,
+  }
+  return {
+    inputAttributes,
+    label,
+  }
+}
+
+type UserForm<T extends object> = {
+  [UserField in keyof typeof User]: any
+}
+
 export default Vue.extend({
   name: 'App',
 
   data() {
     return {
-      user: User,
+      fields: {
+        [User.first_name.field_name]: convertFieldMetadataToInputData(User.first_name),
+        [User.last_name.field_name]: convertFieldMetadataToInputData(User.last_name),
+        [User.email.field_name]: convertFieldMetadataToInputData(User.email),
+        [User.username.field_name]: convertFieldMetadataToInputData(User.username),
+      },
+      form: {},
     }
+  },
+
+  methods: {
+    onSubmit() {
+      console.info({...this.form})
+    },
   },
 });
 </script>
@@ -38,8 +65,15 @@ export default Vue.extend({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+
+input {
+  margin-bottom: 1rem;
+}
+
+label {
+  display: block;
 }
 </style>
